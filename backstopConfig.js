f38a4c427946adb6fa9764e15891578902f097a2
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-06-19 08:59:54 -0300

    added solutions

--- backstopConfig.js
+++ backstopConfig.js
@@ -15,6 +15,10 @@ const config = {
   fileNameTemplate: '{scenarioLabel}_{viewportLabel}',
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
+  engineOptions: {
+
+    executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+  },
   viewports: [
     {
       name: '1024px',
